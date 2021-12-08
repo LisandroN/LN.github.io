a707acabc7257c69420124db08069d61aadc849e
Lisandro Neris <lisandroneris07@gmail.com>
2021-12-07 22:35:45 -0500

    Created working Pages

---
+++ src/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Nav from './Nav';
+
+import './App.css';
+
+function Contact() {
+
+
+  return (
+    <div>
+      <Nav/>
+      <h1>Contact Page</h1>
+    </div>
+  );
+}
+
+export default Contact;
